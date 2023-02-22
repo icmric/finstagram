2301f81c0e2e74d002ba15eebeeaaabf4f467373
Eric McClelland <eric.mcclelland@stu.pacifichills.nsw.edu.au>
2023-02-22 19:46:42 +1100

    first commit

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,162 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _registerPageState();
+  }
+}
+
+class _registerPageState extends State<RegisterPage> {
+  double? _deviceHeight, _deviceWidth;
+  final GlobalKey<FormState> _registerFormKey = GlobalKey<FormState>();
+  String? _name, _email, _password;
+  File? _image;
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: _deviceWidth! * 0.05),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _titleWidget(),
+                _registionForm(),
+                _registerButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _titleWidget() {
+    return const Text(
+      "Finstagram",
+      style: TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _registionForm() {
+    return Container(
+      height: _deviceHeight! * 0.4,
+      child: Form(
+        key: _registerFormKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _profileImageWidget(),
+            _nameTextField(),
+            _emailTextField(),
+            _passwordTextField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _nameTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: "Name..."),
+      validator: (_value) =>
+          _value!.length > 0 ? null : "Please enter a name...",
+      onSaved: (_value) {
+        setState(() {
+          _name = _value;
+        });
+      },
+    );
+  }
+
+  Widget _profileImageWidget() {
+    var _imgProvider = _image != null
+        ? FileImage(_image!)
+        : const NetworkImage("https://i.pravatar.cc/300");
+    return GestureDetector(
+      onTap: () {
+        FilePicker.platform.pickFiles(type: FileType.image).then((_result) {
+          setState(() {
+            _image = File(_result!.files.first.path!);
+          });
+        });
+      },
+      child: Container(
+        height: _deviceHeight! * 0.15,
+        width: _deviceHeight! * 0.15,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: _imgProvider as ImageProvider,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _emailTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: "E-mail..."),
+      validator: (_value) =>
+          _value!.length > 0 ? null : "Please enter an Email adress...",
+      onSaved: (_value) {
+        setState(() {
+          _email = _value;
+        });
+      },
+    );
+  }
+
+  Widget _passwordTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: "Password..."),
+      validator: (_value) =>
+          _value!.length > 0 ? null : "Please enter a Password...",
+      onSaved: (_value) {
+        setState(() {
+          _password = _value;
+        });
+      },
+    );
+  }
+
+  Widget _registerButton() {
+    return MaterialButton(
+      onPressed: () {
+        _registerUser();
+      },
+      minWidth: _deviceWidth! * 0.5,
+      height: _deviceHeight! * 0.05,
+      color: Colors.red,
+      child: const Text(
+        "Register",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+
+  void _registerUser() {
+    if (_registerFormKey.currentState!.validate() && _image != null) {
+      _registerFormKey.currentState!.save();
+      print("Saved");
+    }
+  }
+}
